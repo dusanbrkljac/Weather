6447b3994af415bdab1ca1b26f1d042cf2530ef0
Dusan <dusan.nk@gmail.com>
2022-11-23 10:16:44 +0100

    add

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,8 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/weather/'
-//     : '/'
-// }
+
 
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // transpileDependencies: true
+  transpileDependencies: true,
   publicPath: '/weather/'
 
 })
